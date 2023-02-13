6a2f86cdb5bfa2d6066bbf753fcb93d626b11595
Laurent Sewer <sewer.laurent@hotmail.com>
2023-02-13 11:31:36 +0100

    Master

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,11 @@
 import svgLoader from "vite-svg-loader"
 
 export default defineNuxtConfig({
-	devServer: {     
-    port: 8000, // default: 3000     
-    host: '172.22.22.55', // default: localhost   
-  },   
 	vite: {
 		plugins: [svgLoader()]
 	},
 	css: [
-    `~/assets/sass/index.sass`,
+		`~/assets/sass/index.sass`,
 	],
 	imports: {
 		dirs: [
@@ -19,6 +15,7 @@ export default defineNuxtConfig({
 		]
 	},
 	app: {
+		baseURL: 'https://orlaurack.github.io/',
 		head: {
 			meta: [
 				{ name: 'viewport', content: 'width=device-width, initial-scale=1' }
